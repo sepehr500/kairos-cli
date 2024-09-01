4ea2e5b14d95e3a45f05931711e79f17dfde7dec
Sepehr Sobhani <sepehr411@gmail.com>
2024-08-31 21:56:20 -0400

    Do more focus box work

--- focused_mode_view.go
+++ focused_mode_view.go
@@ -10,16 +10,22 @@ var bottomBoxStyle = lipgloss.NewStyle().Border(lipgloss.RoundedBorder())
 
 // Each border is .5 characters wide, so we subtract 2 from the width and height
 func (m model) focusedModeView() string {
-	boxWidth := (m.viewport.Width / 2) - 2
-	boxHeight := (m.viewport.Height * 2 / 3) - 2
+	var selectedWorkflow *workflowTableListItem = nil
+	for _, workflow := range m.workflows {
+		if workflow.workflow.Execution.WorkflowId == m.focusViewWorkflowId {
+			selectedWorkflow = workflow
+			break
+		}
+	}
 
-	leftBox := leftBoxStyle.Width(boxWidth).Height(boxHeight).Render("Left box")
-
-	// Right box
-	rightBox := rightBoxStyle.Width(boxWidth).Height(boxHeight).Render("Right box")
+	boxWidth := m.viewport.Width / 2
+	boxHeight := m.viewport.Height * 2 / 3
+	leftBoxStyle := leftBoxStyle.Width(boxWidth).Height(boxHeight).Padding(0, 0).Margin(0, 0)
+	x, y := leftBoxStyle.GetFrameSize()
+	rightBoxStyle := rightBoxStyle.Width(boxWidth-x*2).Height(boxHeight).Padding(0, 0).Margin(0, 0)
 
 	// Bottom box
-	bottomBox := bottomBoxStyle.Width(m.viewport.Width - 3).Height(m.viewport.Height - boxHeight - 4).Render("Bottom box")
-	return lipgloss.JoinVertical(lipgloss.Left, lipgloss.JoinHorizontal(lipgloss.Left, leftBox, rightBox), bottomBox)
+	bottomBoxStyle := bottomBoxStyle.Width(m.viewport.Width - x).Height(m.viewport.Height - boxHeight - y*2)
+	return lipgloss.JoinVertical(lipgloss.Left, lipgloss.JoinHorizontal(lipgloss.Left, leftBoxStyle.Render(selectedWorkflow.workflow.String()), rightBoxStyle.Render("right box")), bottomBoxStyle.Render("bottom box"))
 
 }
